fd3d27068adb35d005e0e596149d40766b39ef8a
Sondre Wabakken Engell <sondre.engell@bouvet.no>
2016-10-31 07:53:08 +0100

    Lagt til bilde som felt på Contact.
    - Nytt felt Bitmap picture (med getter og setter)
    - Ny konstruktør som tar inn alle feltene, inkl picture

--- app/src/main/java/no/bouvet/androidskolen/nearbycontacts/models/Contact.java
+++ app/src/main/java/no/bouvet/androidskolen/nearbycontacts/models/Contact.java
@@ -1,5 +1,7 @@
 package no.bouvet.androidskolen.nearbycontacts.models;
 
+import android.graphics.Bitmap;
+
 import com.google.gson.Gson;
 
 public class Contact {
@@ -9,8 +11,15 @@ public class Contact {
     private final String name;
     private final String email;
     private final String telephone;
+    private Bitmap picture;
 
     public Contact(String name, String email, String telephone) {
+        this.name = name;
+        this.email = email;
+        this.telephone = telephone;
+    }
+
+    public Contact(String name, String email, String telephone, Bitmap picture) {
         this.name = name;
         this.email = email;
         this.telephone = telephone;
@@ -26,6 +35,14 @@ public class Contact {
 
     public String getTelephone() {
         return telephone;
+    }
+
+    public Bitmap getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Bitmap picture) {
+        this.picture = picture;
     }
 
     @Override
